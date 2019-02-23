fc7389099909de6610282ca0f64cfa39164e035a
Samir Wadhwania <swadhwania96@gmail.com>
2019-02-23 14:40:37 -0500

    down to policy

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, meta
                     help='Temperature parameter α automaically adjusted.')
 parser.add_argument('--seed', type=int, default=456, metavar='N',
                     help='random seed (default: 456)')
-parser.add_argument('--batch_size', type=int, default=256, metavar='N',
-                    help='batch size (default: 256)')
+parser.add_argument('--batch_size', type=int, default=1024, metavar='N',
+                    help='batch size (default: 1024)')
 parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
                     help='maximum number of steps (default: 1000000)')
 parser.add_argument('--max_ep_length', type=int, default=25, metavar='N',
@@ -52,6 +52,8 @@ parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
 parser.add_argument('--flip_ep', type=int, default=1000000, metavar='N',
                     help='episde after which to use flipped environment (default: 10000000)')
 parser.add_argument('--n_rollout_threads', type=int, default=1, metavar='N',
+                    help='multithreading')
+parser.add_argument('--n_training_threads', type=int, default=6, metavar='N',
                     help='multithreading') 
 args = parser.parse_args()
 
@@ -71,6 +73,7 @@ def make_parallel_env(env_id, n_rollout_threads, seed, discrete_action):
 
 torch.manual_seed(args.seed)
 np.random.seed(args.seed)
+torch.set_num_threads(args.n_training_threads)
 
 # Environment
 env = make_parallel_env(args.env_name, args.n_rollout_threads, args.seed, False)
@@ -84,7 +87,7 @@ replay_buffer = ReplayBuffer(args.replay_size, sac.nagents,
                       [acsp.shape[0] for acsp in env.action_space])
 
 # Training Loop
-rewards = []
+total_rewards = []
 test_rewards = []
 total_numsteps = 0
 updates = 0
@@ -96,8 +99,7 @@ for i_episode in itertools.count():
         print("Flipping")
         replay_buffer = ReplayBuffer(config.buffer_length, sac.nagents,
                              [obsp.shape[0] for obsp in env.observation_space],
-                             [acsp.shape[0] if isinstance(acsp, Box) else acsp.n
-                              for acsp in env.action_space])
+                             [acsp.shape[0] for acsp in env.action_space])
         flip = True
 
     obs = env.reset(flip=flip)
@@ -111,62 +113,55 @@ for i_episode in itertools.count():
                                   requires_grad=False)
                          for i in range(sac.nagents)]
 
-        if args.start_steps > total_numsteps:
+        if args.start_steps < total_numsteps:
             torch_agent_actions = env.sample_action_spaces() # Sample action from env
         else:
-            torch_agent_actions = agent.select_action(obs)  # Sample action from policy
-
+            torch_agent_actions = sac.step(torch_obs)  # Sample action from policy
         # convert actions to numpy arrays
         agent_actions = [ac.data.numpy() for ac in torch_agent_actions]
-        # rearrange actions to be per environment
-        actions = [[ac[i] for ac in agent_actions] for i in range(args.n_rollout_threads)]
 
-        next_obs, rewards, dones, infos = env.step(actions)
+        next_obs, rewards, dones, infos = env.step([agent_actions])
         if ep_step == args.max_ep_length-1:
             dones = dones+1
 
-        if (i_episode % 100) == 0:
+        if (i_episode % 10) == 0:
             time.sleep(0.01)
             env.render()
 
         replay_buffer.push(obs, agent_actions, rewards, next_obs, dones)
-        obs = next_obs
-        total_numsteps += args.n_rollout_threads
 
         if len(replay_buffer) > args.batch_size:
             # Number of updates per step in environment
             for i in range(args.updates_per_step):
-                for a_i in range(sac.agents):
+                for a_i in range(sac.nagents):
                     # Sample a batch from memory
                     sample = replay_buffer.sample(args.batch_size)
-                    # Update parameters of all the networks
-                    value_loss, critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(obs_batch, action_batch,
-                                                                                                                     reward_batch, next_obs_batch,
-                                                                                                                     mask_batch, updates)
+                    # Update parameters of all the networks for each agent
+                    value_loss, critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = sac.update_parameters(a_i, sample, updates)
 
-                    writer.add_scalar('loss/value', value_loss, updates)
-                    writer.add_scalar('loss/critic_1', critic_1_loss, updates)
-                    writer.add_scalar('loss/critic_2', critic_2_loss, updates)
-                    writer.add_scalar('loss/policy', policy_loss, updates)
-                    writer.add_scalar('loss/entropy_loss', ent_loss, updates)
-                    writer.add_scalar('entropy_temprature/alpha', alpha, updates)
-                    updates += 1
+                    writer.add_scalar('agent_{}/loss/value'.format(a_i), value_loss, updates)
+                    writer.add_scalar('agent_{}/loss/critic_1'.format(a_i), critic_1_loss, updates)
+                    writer.add_scalar('agent_{}/loss/critic_2'.format(a_i), critic_2_loss, updates)
+                    writer.add_scalar('agent_{}/loss/policy'.format(a_i), policy_loss, updates)
+                    writer.add_scalar('agent_{}/loss/entropy_loss'.format(a_i), ent_loss, updates)
+                    writer.add_scalar('agent_{}/entropy_temprature/alpha'.format(a_i), alpha, updates)
+                updates += 1    
 
         obs = next_obs
-        total_numsteps += 1
-        episode_reward += reward
+        total_numsteps += args.n_rollout_threads
+        episode_reward += np.sum(rewards)/sac.nagents
         ep_step += 1
 
-        if done:
+        if dones.all():
             break
 
     if total_numsteps > args.num_steps:
         break
 
     writer.add_scalar('reward/train', episode_reward, i_episode)
-    rewards.append(episode_reward)
-    print("Episode: {}, total numsteps: {}, reward: {}, average reward: {}".format(i_episode, total_numsteps, np.round(rewards[-1], 2),
-                                                                                   np.round(np.mean(rewards[-100:]), 2)))
+    total_rewards.append(episode_reward)
+    print("Episode: {}, total numsteps: {}, reward: {}, average reward: {}".format(i_episode, total_numsteps, np.round(total_rewards[-1], 2),
+                                                                                   np.round(np.mean(total_rewards[-100:]), 2)))
 
     if i_episode % 10 == 0 and args.eval == True:
         state = torch.Tensor([env.reset()])
