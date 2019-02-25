fd321fdaa5ee660bf2be11960a3b8c88faad4901
Dong Ki Kim <dkkim93@mit.edu>
2019-02-24 20:51:43 -0500

    venv requirement and train script added

---
+++ _train.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function print_header(){
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    echo $1
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
+# Directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+
+# Tensorboard
+pkill tensorboard
+
+# Virtualenv
+cd $DIR
+virtualenv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+# Add baseline package to path
+export PYTHONPATH=$DIR/thirdparty/multiagent-particle-envs:$PYTHONPATH
+
+# Train tf 
+print_header "Training network"
+cd $DIR
+
+# Comment for using GPU
+export CUDA_VISIBLE_DEVICES=-1
+
+# Experiment 1
+python3.6 main.py simple_spread_flip test \
+--seed 1 \
+--num_eps 5000 \
+--flip_ep 1500 \
+--distill_ep 1500 \
+--log_comment \
